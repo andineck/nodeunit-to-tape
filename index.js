bdefb745aead9bb2b9186c9f850c29d8546240bb
andi-neck <andi.neck@intesso.com>
2015-04-07 23:49:01 +0200

    adds tests, tape bugfix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var path = require('path');
 var es = require('event-stream');
+
 var input, output;
 var separator = '\n';
 
@@ -18,11 +18,11 @@ if (!module.parent) {
 
 module.exports = convertStream;
 function convertStream(input, output) {
-  var p = path.resolve(__dirname + '/../../') + '/';
-  var test = 'var test = require(\'tape\');\n';
+
+  var test = 'var testest = require(\'tape\');\n';
   var l = 0;
 
-  fs.createReadStream(p + input, {flags: 'r'})
+  fs.createReadStream(input, {flags: 'r'})
     .pipe(es.split(separator))
     .pipe(es.map(function(line, cb) {
       if (l == 0) {
@@ -40,6 +40,7 @@ function convertStream(input, output) {
     .pipe(es.replace('exports[', 'test('))
     .pipe(es.replace('exports.', 'test('))
     .pipe(es.replace('] = function', ', function'))
-    .pipe(fs.createWriteStream(p + output));
+
+    .pipe(fs.createWriteStream(output));
 
 }
---
+++ test/test-input.js
@@ -0,0 +1,259 @@
+var async = require('../series');
+
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (thisArg) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var self = this;
+        return function () {
+            self.apply(thisArg, args.concat(Array.prototype.slice.call(arguments)));
+        }
+    };
+}
+
+function eachIterator(args, x, callback) {
+    setTimeout(function(){
+        args.push(x);
+        callback();
+    }, x*25);
+}
+
+function mapIterator(call_order, x, callback) {
+    setTimeout(function(){
+        call_order.push(x);
+        callback(null, x*2);
+    }, x*25);
+}
+
+function filterIterator(x, callback) {
+    setTimeout(function(){
+        callback(x % 2);
+    }, x*25);
+}
+
+function detectIterator(call_order, x, callback) {
+    setTimeout(function(){
+        call_order.push(x);
+        callback(x == 2);
+    }, x*25);
+}
+
+function eachNoCallbackIterator(test, x, callback) {
+    test.equal(x, 1);
+    callback();
+    test.done();
+}
+
+function getFunctionsObject(call_order) {
+    return {
+        one: function(callback){
+            setTimeout(function(){
+                call_order.push(1);
+                callback(null, 1);
+            }, 125);
+        },
+        two: function(callback){
+            setTimeout(function(){
+                call_order.push(2);
+                callback(null, 2);
+            }, 200);
+        },
+        three: function(callback){
+            setTimeout(function(){
+                call_order.push(3);
+                callback(null, 3,3);
+            }, 50);
+        }
+    };
+}
+
+exports['series function alias'] = function(test){
+  var call_order = [];
+  async([
+      function(callback){
+        setTimeout(function(){
+          call_order.push(1);
+          callback(null, 1);
+        }, 25);
+      },
+      function(callback){
+        setTimeout(function(){
+          call_order.push(2);
+          callback(null, 2);
+        }, 50);
+      },
+      function(callback){
+        setTimeout(function(){
+          call_order.push(3);
+          callback(null, 3,3);
+        }, 15);
+      }
+    ],
+    function(err, results){
+      test.equals(err, null);
+      test.same(results, [1,2,[3,3]]);
+      test.same(call_order, [1,2,3]);
+      test.done();
+    });
+};
+
+exports['series'] = function(test){
+    var call_order = [];
+    async.series([
+        function(callback){
+            setTimeout(function(){
+                call_order.push(1);
+                callback(null, 1);
+            }, 25);
+        },
+        function(callback){
+            setTimeout(function(){
+                call_order.push(2);
+                callback(null, 2);
+            }, 50);
+        },
+        function(callback){
+            setTimeout(function(){
+                call_order.push(3);
+                callback(null, 3,3);
+            }, 15);
+        }
+    ],
+    function(err, results){
+        test.equals(err, null);
+        test.same(results, [1,2,[3,3]]);
+        test.same(call_order, [1,2,3]);
+        test.done();
+    });
+};
+
+exports['series empty array'] = function(test){
+    async.series([], function(err, results){
+        test.equals(err, null);
+        test.same(results, []);
+        test.done();
+    });
+};
+
+exports['series error'] = function(test){
+    test.expect(1);
+    async.series([
+        function(callback){
+            callback('error', 1);
+        },
+        function(callback){
+            test.ok(false, 'should not be called');
+            callback('error2', 2);
+        }
+    ],
+    function(err, results){
+        test.equals(err, 'error');
+    });
+    setTimeout(test.done, 100);
+};
+
+exports['series no callback'] = function(test){
+    async.series([
+        function(callback){callback();},
+        function(callback){callback(); test.done();},
+    ]);
+};
+
+exports['series object'] = function(test){
+    var call_order = [];
+    async.series(getFunctionsObject(call_order), function(err, results){
+        test.equals(err, null);
+        test.same(results, {
+            one: 1,
+            two: 2,
+            three: [3,3]
+        });
+        test.same(call_order, [1,2,3]);
+        test.done();
+    });
+};
+
+exports['series call in another context'] = function(test) {
+    if (typeof process === 'undefined') {
+        // node only test
+        test.done();
+        return;
+    }
+    var vm = require('vm');
+    var sandbox = {
+        async: async,
+        test: test
+    };
+
+    var fn = "(" + (function () {
+        async.series([function (callback) {
+            callback();
+        }], function (err) {
+            if (err) {
+                return test.done(err);
+            }
+            test.done();
+        });
+    }).toString() + "())";
+
+    vm.runInNewContext(fn, sandbox);
+};
+
+exports['eachSeries'] = function(test){
+    var args = [];
+    async.eachSeries([1,3,2], eachIterator.bind(this, args), function(err){
+        test.same(args, [1,3,2]);
+        test.done();
+    });
+};
+
+exports['eachSeries empty array'] = function(test){
+    test.expect(1);
+    async.eachSeries([], function(x, callback){
+        test.ok(false, 'iterator should not be called');
+        callback();
+    }, function(err){
+        test.ok(true, 'should call callback');
+    });
+    setTimeout(test.done, 25);
+};
+
+exports['eachSeries error'] = function(test){
+    test.expect(2);
+    var call_order = [];
+    async.eachSeries([1,2,3], function(x, callback){
+        call_order.push(x);
+        callback('error');
+    }, function(err){
+        test.same(call_order, [1]);
+        test.equals(err, 'error');
+    });
+    setTimeout(test.done, 50);
+};
+
+exports['eachSeries no callback'] = function(test){
+    async.eachSeries([1], eachNoCallbackIterator.bind(this, test));
+};
+
+exports['forEachSeries alias'] = function (test) {
+    test.strictEqual(async.eachSeries, async.forEachSeries);
+    test.done();
+};
+
+exports['mapSeries'] = function(test){
+    var call_order = [];
+    async.mapSeries([1,3,2], mapIterator.bind(this, call_order), function(err, results){
+        test.same(call_order, [1,3,2]);
+        test.same(results, [2,6,4]);
+        test.done();
+    });
+};
+
+exports['mapSeries error'] = function(test){
+    test.expect(1);
+    async.mapSeries([1,2,3], function(x, callback){
+        callback('error');
+    }, function(err, results){
+        test.equals(err, 'error');
+    });
+    setTimeout(test.done, 50);
+};
